fb59f53620739c871606ab1bc799fceb00c6efa2
dimasyudistira4657-commits <dimasyudistira4657@gmail.com>
2026-09-21 02:40:51 +0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+Uang Mudah V10 Sederhana
+Admin edit data di Supabase Table Editor.
+Kolom tabel nasabah: nama_nasabah, nomor_va, nominal_tagihan, limit_pinjaman, jatuh_tempo, status_pembayaran, bukti_pembayaran.
---
+++ supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL='https://shtwukmvaupffnljpmjc.supabase.co';
+const SUPABASE_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodHd1a212YXVwZmZubGpwbWpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5MTYyNjksImV4cCI6MjEwNTQ5MjI2OX0.c86F2rukAPkj9sfh4dyjvGug97BW_83OTg2Q8VybI80';
+const supabase=window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
